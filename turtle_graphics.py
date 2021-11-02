1c534ffe32f8d084993f007bce16cc0d25ec6cbf
KovalevaValeria <90349748+KovalevaValeria@users.noreply.github.com>
2021-11-02 20:19:47 +0300

    Update turtle_graphics.py

--- turtle_graphics.py
+++ turtle_graphics.py
@@ -20,4 +20,34 @@ for x in range(50):
     t.pencolor(colors[x%4])
     t.forward(x)
     t.right(91)
+
+for x in range(26):
+    t.pencolor(colors[x%4])
+    t.forward(x)
+    t.left(91)
     
+for x in range(50):
+    t.pencolor(colors[x%4])
+    t.forward(x)
+    t.right(91)
+
+
+for x in range(101):
+    t.pencolor(colors[x%4])
+    t.forward(x)
+    t.left(91)
+
+for x in range(150):
+    t.pencolor(colors[x%4])
+    t.forward(x)
+    t.right(91)
+    
+for x in range(101):
+    t.pencolor(colors[x%4])
+    t.forward(x)
+    t.left(91)
+
+for x in range(50):
+    t.pencolor(colors[x%4])
+    t.forward(x)
+    t.right(91)
