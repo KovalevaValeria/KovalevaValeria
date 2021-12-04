87ccf9d9d24ad322dd26bdf78944cec2c3448dce
KovalevaValeria <90349748+KovalevaValeria@users.noreply.github.com>
2021-12-04 20:57:46 +0300

    Create kepler_universe.py

---
+++ kepler_universe.py
@@ -0,0 +1,126 @@
+from abc import ABC, abstractmethod
+import numpy as np
+from numpy import array as vec
+import numpy.linalg
+import matplotlib.pyplot as plt
+import itertools
+
+MODEL_G = 0.5  # гравитационная постоянная
+COLLISION_DISTANCE = 5.0
+COLLISION_COEFFICIENT = 50.0
+MODEL_DELTA_T = 0.01
+TIME_TO_MODEL = 200
+
+
+# ABC это не алфавит, а AbstractBaseClass. Не даст создать экземпляр, пока не переопределишь все методы-заглушки
+class Universe(ABC):
+    """Невнятная вселенная, основа всех миров"""
+
+    def __init__(self):
+        self.bodies = []
+
+    @abstractmethod
+    def gravity_flow_dencity_per_1_1(self, dist):
+        """
+        Плотность потока гравитационного поля между двумя
+        единичными массами на заданном расстоянии
+        """
+        ...
+
+    def model_step(self):
+        """Итерация решения задачи Коши. Конечно не присуща вселенной, но тут на своём месте"""
+        for b1, b2 in itertools.product(self.bodies, self.bodies):
+            if b1 != b2:
+                b1.apply_force(b1.force_induced_by_other(b2))
+        for b in self.bodies:
+            b.advance()
+
+    def add_body(self, body):
+        self.bodies.append(body)
+
+
+class MaterialPoint:
+    """Материальная точка, движущаяся по двумерной плоскости"""
+
+    def __init__(self, universe, mass, position, velocity):
+        self.universe = universe
+        self.mass = mass
+        self.position = position
+        self.velocity = velocity
+        universe.add_body(self)
+
+        self.ptrace = [self.position.copy()]
+        self.vtrace = [self.velocity.copy()]
+
+    def force_induced_by_other(self, other):
+        """Сила, с которой другое тело действует на данное"""
+        delta_p = other.position - self.position
+        distance = numpy.linalg.norm(delta_p)  # Евклидова норма (по теореме Пифагора)
+        force_direction = delta_p / distance
+        force = force_direction * self.mass * other.mass * \
+                self.universe.gravity_flow_dencity_per_1_1(distance)
+        return force
+
+    def advance(self):
+        """Перемещаем тело, исходя из его скорости"""
+        self.position += self.velocity * MODEL_DELTA_T
+        self.ptrace.append(self.position.copy())
+        self.vtrace.append(self.velocity.copy())
+
+    def apply_force(self, force):
+        """Изменяем скорость, исходя из силы, действующей на тело"""
+        self.velocity += force * MODEL_DELTA_T / self.mass
+
+
+class Universe2D(Universe):
+    def __init__(self,
+                 G,  # гравитационная постоянная
+                 k,  # коэффициент при упругом соударении
+                 collision_distance  # всё-таки это не точки
+                 ):
+        super().__init__()
+        self.G = G
+        self.k = k
+        self.collision_distance = collision_distance
+
+    def gravity_flow_dencity_per_1_1(self, dist):
+
+        if dist > self.collision_distance:
+            return self.G / dist
+        else:
+            return -self.k / dist
+
+
+u = Universe2D(MODEL_G, COLLISION_COEFFICIENT, COLLISION_DISTANCE)
+
+
+bodies = [
+    MaterialPoint(u, 500., vec([0., 0.]), vec([0., 0.])),
+    MaterialPoint(u, 10., vec([300., 0.]), vec([0., -10.])),
+    MaterialPoint(u, 10., vec([0., 300.]), vec([15., 0.]))
+]
+
+steps = int(TIME_TO_MODEL / MODEL_DELTA_T)
+for stepn in range(steps):
+    u.model_step()
+
+plt.gca().set_aspect('equal')
+
+for b in bodies:
+    plt.plot(*tuple(map(list, zip(*b.ptrace))))
+
+plt.show()
+
+def plt_kepler(same_fig=False):
+    for b in bodies:
+        ds = np.cross(b.ptrace, b.vtrace)
+
+        x = range(0, len(ds))
+        plt.plot(x, ds)
+
+        if not same_fig: 
+            plt.show()
+    if same_fig:
+        plt.show()
+
+plt_kepler()
